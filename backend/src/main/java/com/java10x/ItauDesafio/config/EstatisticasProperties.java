f6106f24d6b67ba4972e76f06edb31f79797f50b
Diego Corte <emptyyyxd@gmail.com>
2026-05-03 00:20:33 -0300

    refactor: add validation annotation to EstatisticasProperties for improved property validation

--- backend/src/main/java/com/java10x/ItauDesafio/config/EstatisticasProperties.java
+++ backend/src/main/java/com/java10x/ItauDesafio/config/EstatisticasProperties.java
@@ -3,8 +3,9 @@ package com.java10x.ItauDesafio.config;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
-
+@Validated // Super necessario para a validacao funcionar
 @ConfigurationProperties(prefix = "estatistica")
 public record EstatisticasProperties(@NotNull @Positive Integer segundos) {
 
