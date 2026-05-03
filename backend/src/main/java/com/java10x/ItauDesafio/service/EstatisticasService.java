c61801118690b528a88b32ed1daf3b4a01721d7d
Diego Corte <emptyyyxd@gmail.com>
2026-05-02 21:53:03 -0300

    feat: add EstatisticasService to calculate transaction statistics

---
+++ backend/src/main/java/com/java10x/ItauDesafio/service/EstatisticasService.java
@@ -0,0 +1,74 @@
+package com.java10x.ItauDesafio.service;
+
+import com.java10x.ItauDesafio.model.EstatisticaDTO;
+import com.java10x.ItauDesafio.model.TransacaoRequest;
+import com.java10x.ItauDesafio.repository.TransacaoRepository;
+import org.springframework.stereotype.Service;
+
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
+@Service
+public class EstatisticasService {
+
+
+    private final TransacaoRepository repository;
+
+    public EstatisticasService(TransacaoRepository repository) {
+        this.repository = repository;
+    }
+
+    public EstatisticaDTO calcularEstatisticas(){
+
+        OffsetDateTime agora = OffsetDateTime.now();
+
+        List<TransacaoRequest> recentes = repository.listar().stream()
+                .filter(t -> t.dataHora().isAfter(agora.minusSeconds(60)))
+                .toList();
+
+        if(recentes.isEmpty()) {
+            return new EstatisticaDTO(
+                    0L,
+                    BigDecimal.ZERO,
+                    BigDecimal.ZERO,
+                    BigDecimal.ZERO,
+                    BigDecimal.ZERO);
+        }
+
+        BigDecimal sum = recentes.stream()
+                .map(TransacaoRequest::valor)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal avg = sum.divide(BigDecimal.valueOf(recentes.size()),
+                2, RoundingMode.HALF_UP);
+
+        BigDecimal max = recentes.stream()
+                .map(TransacaoRequest::valor)
+                .max(BigDecimal::compareTo)
+                .orElse(BigDecimal.ZERO);
+
+        BigDecimal min = recentes.stream()
+                .map(TransacaoRequest::valor)
+                .min(BigDecimal::compareTo)
+                .orElse(BigDecimal.ZERO);
+
+        Long count = (long) recentes.size();
+
+        return new  EstatisticaDTO(
+                count,
+                sum,
+                avg,
+                min,
+                max
+        );
+
+
+    }
+
+
+
+}
