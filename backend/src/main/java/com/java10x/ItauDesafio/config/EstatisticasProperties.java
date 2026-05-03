d0740f10eb459333c90ac1016caa63f217240f18
Diego Corte <emptyyyxd@gmail.com>
2026-05-03 00:17:22 -0300

    refactor: add validation for segundos in EstatisticasProperties

--- backend/src/main/java/com/java10x/ItauDesafio/config/EstatisticasProperties.java
+++ backend/src/main/java/com/java10x/ItauDesafio/config/EstatisticasProperties.java
@@ -1,11 +1,11 @@
 package com.java10x.ItauDesafio.config;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 
 @ConfigurationProperties(prefix = "estatistica")
-public record EstatisticasProperties(@NotBlank Integer segundos) {
+public record EstatisticasProperties(@NotNull @Positive Integer segundos) {
 
 }
