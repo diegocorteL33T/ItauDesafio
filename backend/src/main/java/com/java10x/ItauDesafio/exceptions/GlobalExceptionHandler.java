5a85ee1c33634b160976613db584801afba355ab
Diego Corte <emptyyyxd@gmail.com>
2026-05-02 23:47:59 -0300

    refactor: add logging to GlobalExceptionHandler for improved error tracking

--- backend/src/main/java/com/java10x/ItauDesafio/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/java10x/ItauDesafio/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.java10x.ItauDesafio.exceptions;
 
-import org.springframework.http.HttpMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -8,21 +8,25 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Void> handleValidationErrors(MethodArgumentNotValidException ex) {
+        log.error("Validação falhou: {}",ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Void> handleUnreadableBody(HttpMessageNotReadableException ex) {
+        log.error("Corpo de requisição inválido/ausente: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Void> handleIllegalArgument(IllegalArgumentException ex) {
+        log.error("Validação falhou, regra de negócio inválida: {}",ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
     }
 
