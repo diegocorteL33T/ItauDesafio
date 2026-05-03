4d801a92e28c13c9f56f4f923ea52a832c36532d
Diego Corte <emptyyyxd@gmail.com>
2026-05-02 23:01:27 -0300

    refactor: simplify response handling in EstatisticasController

--- backend/src/main/java/com/java10x/ItauDesafio/controller/EstatisticasController.java
+++ backend/src/main/java/com/java10x/ItauDesafio/controller/EstatisticasController.java
@@ -1,14 +1,12 @@
 package com.java10x.ItauDesafio.controller;
 
 
+import com.java10x.ItauDesafio.config.EstatisticasProperties;
 import com.java10x.ItauDesafio.model.EstatisticaDTO;
 import com.java10x.ItauDesafio.service.EstatisticasService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @RestController
 @RequestMapping("/estatistica")
@@ -22,8 +20,8 @@ public class EstatisticasController {
 
     @GetMapping
     public ResponseEntity<EstatisticaDTO> estatistica() {
-        return ResponseEntity.status(HttpStatus.OK).body(service.calcularEstatisticas());
-
+        EstatisticaDTO stats = service.calcularEstatisticas();
+        return ResponseEntity.status(HttpStatus.OK).body(stats);
     }
 
 }
