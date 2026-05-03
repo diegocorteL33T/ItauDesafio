3b04220a9789df6946aa116a96eff1b656a0d700
Diego Corte <emptyyyxd@gmail.com>
2026-05-02 23:47:50 -0300

    refactor: add logging to EstatisticasController and update constructor for properties injection

--- backend/src/main/java/com/java10x/ItauDesafio/controller/EstatisticasController.java
+++ backend/src/main/java/com/java10x/ItauDesafio/controller/EstatisticasController.java
@@ -4,22 +4,28 @@ package com.java10x.ItauDesafio.controller;
 import com.java10x.ItauDesafio.config.EstatisticasProperties;
 import com.java10x.ItauDesafio.model.EstatisticaDTO;
 import com.java10x.ItauDesafio.service.EstatisticasService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/estatistica")
 public class EstatisticasController {
 
     private final EstatisticasService service;
+    private final EstatisticasProperties estatisticasProperties;
 
-    public EstatisticasController(EstatisticasService service) {
+    public EstatisticasController(EstatisticasService service, EstatisticasProperties estatisticasProperties) {
         this.service = service;
+        this.estatisticasProperties = estatisticasProperties;
     }
 
     @GetMapping
     public ResponseEntity<EstatisticaDTO> estatistica() {
+        // Log de requisicao criado via Lombok
+        log.info("Calculando Estatisticas de trasacacao nos ultimos {} segundos: ", estatisticasProperties.segundos());
         EstatisticaDTO stats = service.calcularEstatisticas();
         return ResponseEntity.status(HttpStatus.OK).body(stats);
     }
