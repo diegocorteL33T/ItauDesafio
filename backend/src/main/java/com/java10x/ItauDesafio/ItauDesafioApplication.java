744d5efc179f020a132b520f9728f5cb1aa9f4f9
Diego Corte <emptyyyxd@gmail.com>
2026-05-02 23:01:45 -0300

    feat: enable configuration properties scanning in ItauDesafioApplication

--- backend/src/main/java/com/java10x/ItauDesafio/ItauDesafioApplication.java
+++ backend/src/main/java/com/java10x/ItauDesafio/ItauDesafioApplication.java
@@ -2,9 +2,12 @@ package com.java10x.ItauDesafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class ItauDesafioApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItauDesafioApplication.class, args);
