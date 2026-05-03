38ecd98ec8628d08dc59da650092f032b72c27e4
Diego Corte <emptyyyxd@gmail.com>
2026-05-02 21:52:52 -0300

    feat: add EstatisticasController to handle statistics calculations

---
+++ backend/src/main/java/com/java10x/ItauDesafio/controller/EstatisticasController.java
@@ -0,0 +1,29 @@
+package com.java10x.ItauDesafio.controller;
+
+
+import com.java10x.ItauDesafio.model.EstatisticaDTO;
+import com.java10x.ItauDesafio.service.EstatisticasService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/estatisticas")
+public class EstatisticasController {
+
+    private final EstatisticasService service;
+
+    public EstatisticasController(EstatisticasService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<EstatisticaDTO> estatistica() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.calcularEstatisticas());
+
+    }
+
+}
