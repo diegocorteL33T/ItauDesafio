dae40d041dfc930ed2c3e3cc944a42f4d80bed54
Diego Corte <emptyyyxd@gmail.com>
2026-05-02 23:47:55 -0300

    refactor: add validation annotations to EstatisticasProperties

--- backend/src/main/java/com/java10x/ItauDesafio/config/EstatisticasProperties.java
+++ backend/src/main/java/com/java10x/ItauDesafio/config/EstatisticasProperties.java
@@ -1,9 +1,11 @@
 package com.java10x.ItauDesafio.config;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 
 @ConfigurationProperties(prefix = "estatistica")
-public record EstatisticasProperties(Integer segundos) {
+public record EstatisticasProperties(@NotBlank Integer segundos) {
 
 }
