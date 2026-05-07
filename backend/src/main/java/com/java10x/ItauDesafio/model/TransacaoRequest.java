2ede6224c77aec6705c882c5a5831cf6b3b252af
Diego Corte <emptyyyxd@gmail.com>
2026-05-07 15:10:15 -0300

    refactor: add positive validation to valor in TransacaoRequest

--- backend/src/main/java/com/java10x/ItauDesafio/model/TransacaoRequest.java
+++ backend/src/main/java/com/java10x/ItauDesafio/model/TransacaoRequest.java
@@ -1,13 +1,14 @@
 package com.java10x.ItauDesafio.model;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 public record TransacaoRequest
         (
-         @NotNull(message = "Valor é obrigatório") BigDecimal valor,
+         @NotNull(message = "Valor é obrigatório") @Positive(message = "Valor deve ser positivo") BigDecimal valor,
          @NotNull(message = "Data e hora são obrigatórias") OffsetDateTime dataHora
         ) {
 
